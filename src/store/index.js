65527b87e6e43af4f4118c381800db4d442cb345
Attila Szeremi <attila@szeremi.org>
2022-04-13 20:56:57 +0200

    Add counter with Vuex

--- src/store/index.js
+++ src/store/index.js
@@ -4,9 +4,15 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    counter: 0,
+  },
   getters: {},
-  mutations: {},
+  mutations: {
+    increment(state) {
+      state.counter++;
+    },
+  },
   actions: {},
   modules: {},
 });
